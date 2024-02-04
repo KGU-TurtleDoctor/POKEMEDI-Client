e5e9975804de0e00cc25e8147442853d891b7ec8
jungwoo3490 <jungwoo3490@naver.com>
2024-02-04 23:36:28 +0900

    📦 [CHORE] 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Router />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
