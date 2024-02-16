897e3c24b8d3127bb6a1218ca1498170b32dccb9
jungwoo3490 <jungwoo3490@naver.com>
2024-02-16 15:54:32 +0900

    📦️ [CHORE] Global Style 초기세팅

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyle = createGlobalStyle`
+
+    ${reset}
+    * {
+        box-sizing: border-box;
+
+        font-family:
+        'Pretendard Variable',
+        Pretendard,
+        -apple-system,
+        BlinkMacSystemFont,
+        system-ui,
+        Roboto,
+        'Helvetica Neue',
+        'Segoe UI',
+        'Apple SD Gothic Neo',
+        'Noto Sans KR',
+        'Malgun Gothic',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        sans-serif;
+    }
+
+    html,
+    body {
+        box-sizing: border-box; /* default */
+        margin: 0 auto;
+
+        font-size: 62.5%;
+
+        background-color: ${theme.colors.backGroundGray};
+    }
+
+    input {
+        outline: none;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+
+        cursor: pointer;
+    }
+
+    button {
+        font: inherit;
+
+        background: none;
+        cursor: pointer;
+        border: none;
+    }
+
+    select {
+        cursor: pointer;
+    }
+`;
+
+export default globalStyle;
