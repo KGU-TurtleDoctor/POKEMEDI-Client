85061e22b6fee94a7e7efb7e032d4ab4149bd5f5
jungwoo3490 <jungwoo3490@naver.com>
2024-02-17 14:36:43 +0900

    📦️ [CHORE] GlobalStyle import 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import GlobalStyle from './styles/GlobalStyle.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
