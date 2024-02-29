45330a3948cd3a9806d42266bfd8d35b67ad3433
jungwoo3490 <jungwoo3490@naver.com>
2024-03-01 04:43:19 +0900

    💡 [FEAT] 메뉴 소개 컨테이너 컴포넌트 퍼블리싱

---
+++ src/components/Home/MenuIntroduction.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import MenuBox from './MenuBox';
+
+function MenuIntroduction() {
+  const [isVisible1, setIsVisible1] = useState(false);
+  const [isVisible2, setIsVisible2] = useState(false);
+  const menuRef1 = useRef(null);
+  const menuRef2 = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const bottom1 = menuRef1.current.getBoundingClientRect().top;
+      const bottom2 = menuRef2.current.getBoundingClientRect().top;
+
+      setIsVisible1(bottom1 < window.innerHeight + 200);
+      setIsVisible2(bottom2 < window.innerHeight + 200);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <MenuIntroductionWrapper>
+      <MenuBox ref={menuRef1} isVisible={isVisible1} type={1} />
+      <MenuBox ref={menuRef2} isVisible={isVisible2} type={2} />
+    </MenuIntroductionWrapper>
+  );
+}
+
+export default MenuIntroduction;
+
+const MenuIntroductionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 8rem;
+
+  width: 100%;
+  margin-top: 15rem;
+  padding-bottom: 15rem;
+`;
