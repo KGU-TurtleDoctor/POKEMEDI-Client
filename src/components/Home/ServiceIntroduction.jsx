bd226366f467a73e1e89b2f5a9eb066d00f4a26c
jungwoo3490 <jungwoo3490@naver.com>
2024-02-29 16:44:31 +0900

    💡 [FEAT] 서비스 소개 컨테이너 컴포넌트 퍼블리싱

---
+++ src/components/Home/ServiceIntroduction.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import back1 from '../../assets/img/background1.jpeg';
+import back2 from '../../assets/img/background2.jpeg';
+
+function ServiceIntroduction() {
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+  const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  return (
+    <ServiceintroductionWrapper>
+      <ServiceIntroMessage>
+        내 손 안의 의사
+        <br />
+        POKEMEDI
+      </ServiceIntroMessage>
+      <ServiceIntroSubMessage>
+        집에서도 편리하게, 포켓메디와 함께
+      </ServiceIntroSubMessage>
+      <ServiceImageContainer>
+        <FirstServiceImage src={back1} />
+        <SecondServiceImage src={back2} />
+      </ServiceImageContainer>
+      {/* <button onClick={() => (window.location.href = kakaoURL)}>
+        카카오 로그인버튼
+      </button> */}
+    </ServiceintroductionWrapper>
+  );
+}
+
+export default ServiceIntroduction;
+
+const ServiceintroductionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+
+  text-align: center;
+`;
+
+const fadeup = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 100%, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
+`;
+
+const fadein = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const ServiceIntroMessage = styled.h1`
+  font-size: 5.5rem;
+  font-weight: 700;
+  line-height: 1.3;
+
+  transition-delay: 1.2s;
+  animation: ${fadeup} 1s ease;
+`;
+
+const ServiceIntroSubMessage = styled.h2`
+  margin-top: 2rem;
+
+  font-size: 2.2rem;
+  font-weight: 500;
+  color: #8898a7;
+
+  opacity: 0; /* 초기에는 숨김 */
+  animation: ${fadeup} 0.8s ease forwards; /* forwards 옵션은 애니메이션 종료 후 상태를 유지하도록 합니다. */
+  animation-delay: 0.8s;
+`;
+
+const ServiceImageContainer = styled.div`
+  position: relative;
+
+  width: 75rem;
+  height: 39rem;
+  margin-top: 7rem;
+`;
+
+const FirstServiceImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 50rem;
+  height: 32rem;
+
+  border-radius: 1.2rem;
+
+  opacity: 0; /* 초기에는 숨김 */
+  animation: ${fadein} 0.8s ease forwards; /* forwards 옵션은 애니메이션 종료 후 상태를 유지하도록 합니다. */
+  animation-delay: 1.5s;
+`;
+
+const SecondServiceImage = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+
+  width: 50rem;
+  height: 32rem;
+
+  border-radius: 1.2rem;
+
+  opacity: 0; /* 초기에는 숨김 */
+  animation: ${fadein} 0.8s ease forwards; /* forwards 옵션은 애니메이션 종료 후 상태를 유지하도록 합니다. */
+  animation-delay: 2.3s;
+`;
+
+const GIFIMG = styled.img`
+  width: 32rem;
+  height: 24rem;
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Common/Header';
+import ServiceIntroduction from '../components/Home/ServiceIntroduction';
 
 function Home() {
   return (
     <HomePageWrapper>
       <Header />
+      <HomePageBodyWrapper>
+        <ServiceIntroduction />
+      </HomePageBodyWrapper>
     </HomePageWrapper>
   );
 }
@@ -15,4 +19,8 @@ export default Home;
 const HomePageWrapper = styled.div`
   width: 100%;
   height: 100vh;
+`;
+
+const HomePageBodyWrapper = styled.div`
+  margin-top: 11rem;
 `;
