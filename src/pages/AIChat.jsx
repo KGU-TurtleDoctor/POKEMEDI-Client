6069ae829b7b640e9c9cea569334bfe05ece0ec3
jungwoo3490 <jungwoo3490@naver.com>
2024-04-19 11:14:36 +0900

    ✏️️ [FIX] console 제거

--- src/pages/AIChat.jsx
+++ src/pages/AIChat.jsx
@@ -20,9 +20,7 @@ function AIChat() {
 
   const handleClickSendButton = () => {
     postChatbotPrompt(chat, (data) => {
-      console.log(data);
       setChatList([...chatList, { id: 1, text: chat }, { id: 2, text: data }]);
-      console.log('완료');
       setChat('');
     });
   };
