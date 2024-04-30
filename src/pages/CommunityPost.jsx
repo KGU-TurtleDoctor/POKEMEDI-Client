642fa7523b3f55a072677ed659db97564bee713d
조주희 <carla_cho@naver.com>
2024-04-30 15:34:06 +0900

    ✏️️ [FIX] 게시물 작성 페이지 UI 수정

--- src/pages/CommunityPost.jsx
+++ src/pages/CommunityPost.jsx
@@ -11,16 +11,20 @@ function CommunityPost() {
           <PostTitleWrapper>
             <PostTitleBox>
               <PostTitle>제목</PostTitle>
-              <PostTextCount>(0 / 50)</PostTextCount>
             </PostTitleBox>
-            <TitleInput type="text" placeholder="제목을 입력해주세요." />
+            <TitleDiv>
+              <TitleInput type="text" placeholder="제목을 입력해주세요." />
+              <PostTextCount>(0 / 50)</PostTextCount>
+            </TitleDiv>
           </PostTitleWrapper>
           <PostTextWrapper>
             <PostTextBox>
               <PostText>본문</PostText>
-              <PostTextCount>(0 / 1000)</PostTextCount>
             </PostTextBox>
-            <TextInput type="text" placeholder="본문을 입력해주세요." />
+            <PostDiv>
+              <TextInput type="text" placeholder="본문을 입력해주세요." />
+              <PostTextCount>(0 / 500)</PostTextCount>
+            </PostDiv>
           </PostTextWrapper>
         </CommunityPostBoxWrapper>
       </CommunityPostBodyWrapper>
@@ -37,7 +41,7 @@ const CommunityPostWrapper = styled.div`
 
 const CommunityPostBodyWrapper = styled.div`
   width: 100%;
-  //height: calc(100vh - 8rem);
+
   padding: 0 37.5rem;
   margin-top: 8rem;
 
@@ -46,8 +50,8 @@ const CommunityPostBodyWrapper = styled.div`
 
 const CommunityPostBoxWrapper = styled.div`
   width: calc(100vw - 75rem);
+  min-height: calc(100vh - 8rem);
   padding: 10rem 20rem 10rem 20rem;
-  height: calc(100vh - 8rem);
 
   background-color: white;
   font-size: 2rem;
@@ -99,33 +103,67 @@ const PostText = styled.div`
   font-size: 3rem;
 `;
 
-const PostTextCount = styled.div`
+const PostTextCount = styled.p`
+  font-size: 1.5rem;
+  color: #9e9e9e;
+`;
+
+const TitleDiv = styled.div`
+  width: 100%;
+  height: 5rem;
+  padding: 0 2rem;
+
+  background-color: #f1f5f9;
+  border: 0.05rem solid #9e9e9e;
   font-size: 2rem;
+  border-radius: 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const TitleInput = styled.input`
-  width: 100%;
-  height: 5rem;
+  width: 80%;
+  height: 4.6rem;
 
   background-color: #f1f5f9;
+  border: none;
+  font-size: 2rem;
+  border-radius: 2rem;
+`;
+
+const PostDiv = styled.div`
+  width: 100%;
+  height: 46.3rem;
+  padding: 2rem;
+
+  background-color: #f1f5f9;
+  border: 0.05rem solid #9e9e9e;
+  border-radius: 1.5rem;
 
   font-size: 2rem;
-  border-radius: 1.5rem;
-  border: 0.05rem solid #9e9e9e;
 
-  padding-left: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 `;
 
 const TextInput = styled.textarea`
   width: 100%;
-  height: 30rem;
-
-  resize: none;
-  padding: 2rem;
-
-  background-color: #f1f5f9;
+  height: 92%;
+  margin-bottom: 3%;
 
   font-size: 2rem;
-  border-radius: 1.5rem;
-  border: 0.05rem solid #9e9e9e;
+  background-color: #f1f5f9;
+
+  resize: none;
+
+  border: none;
+
+  overflow: hidd;
+
+  &:focus {
+    outline: none;
+  }
 `;
