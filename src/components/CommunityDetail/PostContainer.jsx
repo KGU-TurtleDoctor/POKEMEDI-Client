fbede78e28ea45da9e9f59eb31620d06104633cc
jungwoo3490 <jungwoo3490@naver.com>
2024-04-30 19:07:50 +0900

    ✏️️ [FIX] useEffect import 추가

--- src/components/CommunityDetail/PostContainer.jsx
+++ src/components/CommunityDetail/PostContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { IcCalendar, IcUser } from '../../assets/svg/icon';
 import CommentContainer from './CommentContainer';
