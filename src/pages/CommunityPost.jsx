d142e6622198b3428feb1cdb702df38a92590eea
조주희 <carla_cho@naver.com>
2024-04-30 17:39:25 +0900

    ?? [FEAT] 제목 및 본문 글자수 제한 설정

--- src/pages/CommunityPost.jsx
+++ src/pages/CommunityPost.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Common/Header';
 
 function CommunityPost() {
+  const navigate = useNavigate();
+  const [TitleWriting, setTitleWriting] = useState('');
+  const [PostWriting, setPostWriting] = useState('');
+
+  const [isSatisfied, setIsSatisfied] = useState(false);
+
+  const handleChangeTitleInput = (e) => {
+    setTitleWriting(e.target.value);
+  };
+
+  const handleChangePostInput = (e) => {
+    setPostWriting(e.target.value);
+  };
+
+  useEffect(() => {
+    if (TitleWriting[0] === ' ') {
+      setTitleWriting(TitleWriting.substring(1, TitleWriting.length));
+    }
+
+    if (TitleWriting.length > 50) {
+      setTitleWriting(TitleWriting.substring(0, 50));
+    }
+
+    if (PostWriting[0] === ' ') {
+      setPostWriting(PostWriting.substring(1, PostWriting.length));
+    }
+
+    if (PostWriting.length > 500) {
+      setPostWriting(PostWriting.substring(0, 500));
+    }
+
+    if (TitleWriting.trim().length !== 0 && PostWriting.trim().length !== 0) {
+      setIsSatisfied(true);
+    } else {
+      setIsSatisfied(false);
+    }
+  }, [TitleWriting, PostWriting]);
+
+  const handleClickWritingDoneButton = () => {
+    if (isSatisfied) {
+      navigate('/community-list');
+    }
+  };
   return (
     <CommunityPostWrapper>
       <Header />
@@ -13,8 +57,13 @@ function CommunityPost() {
               <PostTitle>제목</PostTitle>
             </PostTitleBox>
             <TitleDiv>
-              <TitleInput type="text" placeholder="제목을 입력해주세요." />
-              <PostTextCount>(0 / 50)</PostTextCount>
+              <TitleInput
+                type="text"
+                placeholder="제목을 입력해주세요."
+                onChange={handleChangeTitleInput}
+                value={TitleWriting}
+              />
+              <TextCount>{`(${TitleWriting.length} / 50)`}</TextCount>
             </TitleDiv>
           </PostTitleWrapper>
           <PostTextWrapper>
@@ -22,10 +71,23 @@ function CommunityPost() {
               <PostText>본문</PostText>
             </PostTextBox>
             <PostDiv>
-              <TextInput type="text" placeholder="본문을 입력해주세요." />
-              <PostTextCount>(0 / 500)</PostTextCount>
+              <TextInput
+                type="text"
+                placeholder="본문을 입력해주세요."
+                onChange={handleChangePostInput}
+                value={PostWriting}
+              />
+              <TextCount>{`(${PostWriting.length} / 500)`}</TextCount>
             </PostDiv>
           </PostTextWrapper>
+          <WritingDoneBox>
+            <WritingDoneButton
+              onClick={handleClickWritingDoneButton}
+              isSatisfied={isSatisfied}
+            >
+              작성 완료
+            </WritingDoneButton>
+          </WritingDoneBox>
         </CommunityPostBoxWrapper>
       </CommunityPostBodyWrapper>
     </CommunityPostWrapper>
@@ -51,10 +113,14 @@ const CommunityPostBodyWrapper = styled.div`
 const CommunityPostBoxWrapper = styled.div`
   width: calc(100vw - 75rem);
   min-height: calc(100vh - 8rem);
-  padding: 10rem 20rem 10rem 20rem;
+  padding: 8rem 18rem 8rem 18rem;
 
   background-color: white;
   font-size: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
 `;
 
 const PostTitleWrapper = styled.div`
@@ -71,8 +137,6 @@ const PostTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2rem;
-
-  margin-top: 5rem;
 `;
 
 const PostTitleBox = styled.div`
@@ -103,7 +167,7 @@ const PostText = styled.div`
   font-size: 3rem;
 `;
 
-const PostTextCount = styled.p`
+const TextCount = styled.p`
   font-size: 1.5rem;
   color: #9e9e9e;
 `;
@@ -166,4 +230,25 @@ const TextInput = styled.textarea`
   &:focus {
     outline: none;
   }
+`;
+
+const WritingDoneBox = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const WritingDoneButton = styled.button`
+  width: 10rem;
+  height: 3.75rem;
+
+  border-radius: 0.7rem;
+
+  font-size: 1.75rem;
+  font-weight: 500;
+
+  background-color: ${({ isSatisfied }) => (isSatisfied ? '#04293f' : 'grey')};
+  color: white;
 `;
