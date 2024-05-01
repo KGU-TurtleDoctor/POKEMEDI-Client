9a2b41f67217d4fb98b558ad6ee370c0e1f2a97c
jungwoo3490 <jungwoo3490@naver.com>
2024-05-01 18:33:44 +0900

    ✏️️ [FIX] useEffect import 추가

--- src/pages/CommunityDetail.jsx
+++ src/pages/CommunityDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { IcSendBlack } from '../assets/svg/icon';
