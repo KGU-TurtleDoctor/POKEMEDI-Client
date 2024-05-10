92ee67a19b02cacc7f5b0832b0d9709c81bab913
조주희 <carla_cho@naver.com>
2024-05-10 18:36:00 +0900

    💡 [FEAT] 나의 채팅리스트 목록 페이지 기능 구현

---
+++ src/components/MyChatList/ChatItem.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+function ChatItem({ id, title, nickname, date }) {
+  const navigate = useNavigate();
+
+  const handleClickPostItem = () => {
+    navigate(`/community/aichat/${id}`);
+  };
+
+  return (
+    <PostWrapper onClick={handleClickPostItem}>
+      <PostTitle>{title}</PostTitle>
+      <PostInfo>
+        <PostNickname>{nickname}</PostNickname>
+        <PostDate>{date}</PostDate>
+      </PostInfo>
+    </PostWrapper>
+  );
+}
+
+export default ChatItem;
+
+const PostWrapper = styled.div`
+  width: 100%;
+  padding: 3.2rem 3.2rem;
+
+  background-color: rgb(245, 245, 247);
+
+  border-radius: 1rem;
+  cursor: pointer;
+
+  transition: 0.3s ease;
+
+  &:hover {
+    background-color: #ddd;
+  }
+`;
+
+const PostTitle = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #333;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+`;
+
+const PostInfo = styled.div`
+  column-gap: 1.5rem;
+  margin-top: 2rem;
+
+  display: flex;
+`;
+
+const PostNickname = styled.div`
+  font-size: 1.5rem;
+  color: #777;
+`;
+
+const PostDate = styled.div`
+  font-size: 1.5rem;
+  color: #777;
+`;
