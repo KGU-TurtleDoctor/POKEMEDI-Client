347beef403184fcab1e5f4f23d71879e24842c8c
jungwoo3490 <jungwoo3490@naver.com>
2024-06-06 19:25:10 +0900

    ✏️️ [FIX] font 적용 오류 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -6,27 +6,11 @@ const GlobalStyle = createGlobalStyle`
     ${reset}
     * {
         box-sizing: border-box;
-
-        font-family:
-        'Pretendard',
-        Pretendard,
-        -apple-system,
-        BlinkMacSystemFont,
-        system-ui,
-        Roboto,
-        'Helvetica Neue',
-        'Segoe UI',
-        'Apple SD Gothic Neo',
-        'Noto Sans KR',
-        'Malgun Gothic',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        sans-serif;
     }
 
     html,
     body {
+        font-family: Pretendard;
         box-sizing: border-box; /* default */
         margin: 0 auto;
 
