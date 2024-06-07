a83dc791c45ed1fdeed32a1aafb414361122430a
jungwoo3490 <jungwoo3490@naver.com>
2024-06-07 10:35:57 +0900

    ✏️️ [FIX] useState import 누락 수정

--- src/pages/NotLogin.jsx
+++ src/pages/NotLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ImgError from '../assets/img/img_error.png';
 import Header from '../components/Common/Header';
