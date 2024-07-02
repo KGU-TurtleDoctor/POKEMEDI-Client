00582b37f204dcf353e4d927c61fa1c280c6adb7
jungwoo3490 <jungwoo3490@naver.com>
2024-07-03 00:08:26 +0900

    👀 [TEST] console문 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,8 @@ import { api } from '../libs/api';
 function Home() {
   const [nickname, setNickname] = useState(null);
 
+  console.log('test');
+
   useEffect(() => {
     api.get('/api/isLogin', { withCredentials: true }).then((res) => {
       if (res.data.result.loginStatus) {
